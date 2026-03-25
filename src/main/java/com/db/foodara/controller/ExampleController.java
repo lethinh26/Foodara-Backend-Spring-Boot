f0a5ee1f3b56686f1d0ff0bded76f91b751bf654
lethinh <lephuthinh387@gmail.com>
2026-03-25 20:45:43 +0700

    fix path example

--- src/main/java/com/db/foodara/controller/ExampleController.java
+++ src/main/java/com/db/foodara/controller/ExampleController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/example")
 public class ExampleController {
     @Autowired
     private ExampleService exampleService;
